c571f5c4411cbe9d29406ae197e4e1fee6103d54
Mark Boudreau <mboudreaudalton@gmail.com>
2023-03-25 21:26:18 -0400

    fully allocate the array

--- lib/util/structs.go
+++ lib/util/structs.go
@@ -3,9 +3,9 @@ package util
 import "container/list"
 
 func ListToArray[T any](list *list.List) []T {
-	array := make([]T, 0, list.Len())
-	for cur := list.Front(); cur != nil; cur = cur.Next() {
-		array = append(array, cur.Value.(T))
+	array := make([]T, list.Len())
+	for cur, i := list.Front(), 0; cur != nil; cur, i = cur.Next(), i+1 {
+		array[i] = cur.Value.(T)
 	}
 	return array
 }
